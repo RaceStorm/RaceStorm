ec757d08820955324eaf6bc0354ba586498c25ea
RaceStorm <race.storm@gmx.de>
2016-11-09 16:13:47 +0100

    Test commit and push

--- app/src/main/java/com/racestorm/racestorm/MainActivity.java
+++ app/src/main/java/com/racestorm/racestorm/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("Hi was geht?");
+        System.out.println("Hi was geht?"); //test ausgabe
 
 
 

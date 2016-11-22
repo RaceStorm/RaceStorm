05f6639c2464ec3bc5889e8dd99e287a884093f6
RaceStorm <race.storm@gmx.de>
2016-11-22 23:49:50 +0100

    added bluetooth() methode

--- app/src/main/java/com/racestorm/racestorm/MainActivity.java
+++ app/src/main/java/com/racestorm/racestorm/MainActivity.java
@@ -2,6 +2,8 @@ package com.racestorm.racestorm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,9 +12,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Initialisierung der Buttons der main_activity
+        Button btnStart = (Button) findViewById(R.id.btnStart);
+        Button btnOptions = (Button) findViewById(R.id.btnOptions);
+        Button btnAbout = (Button) findViewById(R.id.btnAbout);
 
+        //Aktion bei klickens des Start Buttons
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bluetooth();
+
+            }
+        });
 
 
 
     }
+
+    //Methode bluetooth (managed die Bluetooth verbindung)
+    public void bluetooth(){
+        setContentView(R.layout.bluetooth);
+
+    }
+
+
+
 }
